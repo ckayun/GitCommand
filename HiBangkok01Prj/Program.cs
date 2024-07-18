a81836e4ccdd6aa0d45cd4512e96ac67f82100f8
Kayun Chantarasathaporn <dr.kayun@gmail.com>
2024-07-18 21:14:09 +0000

    19 jul 2024 04:14

---
+++ HiBangkok01Prj/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+namespace HiBangkok01Prj {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hi Bangkok.");
+            Console.WriteLine("Now it is {0}.", DateTime.Now.ToString());
+        }
+    }
+}
